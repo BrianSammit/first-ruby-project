1f6593ed8915824c6b9b8fbff839524a3cb9bb5a
janis_jenny <pcardenasvm@gmail.com>
2020-09-17 15:34:18 -0500

    fix linters

--- buble.rb
+++ buble.rb
@@ -2,19 +2,19 @@ def buble_sort(arr = [3, 0, 5, 6, 3, 9, 0, 4])
   i = 0
   a = 1
   arrl = arr.length - 1
-  while a != 0  do
+  while a !=0  
     a = 0
     i = 0
-    while i < arrl do
+    while i < arrl 
       x = arr[i]
       y = arr[i + 1]
       if x > y
         arr[i] = y
-        arr[i + 1] = x    
-        a = a + 1
+        arr[i + 1] = x
+        a += 1
       end
-      i = i + 1
-    end 
+      i += 1
+    end
   end
   print arr
   return arr
