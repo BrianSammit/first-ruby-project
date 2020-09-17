87f1e87c82f42706764e3bbf59c8b2f0ff03ffc4
Nexch <mauricio.t.1999@gmail.com>
2020-09-17 14:09:31 -0400

    final only linters left

--- buble.rb
+++ buble.rb
@@ -7,11 +7,30 @@ def buble_sort(arr = [1, 0, 4, 3, 5, 1, 0, 1])
     if x > y
       arr[i] = y
       arr[i + 1] = x
-      puts 'aaa ${arr}'
     end
     i = i + 1
   end
-  return arr
+  print arr
+end
+
+def bubble_sort_by(arr2)
+  i = 0
+  arr2l = arr2.length - 1
+  while i < arr2l do
+    value = yield(arr2[i], arr2[i + 1])
+    if value.positive?
+      x = arr2[i]
+      y = arr2[i + 1]
+      arr2[i] = y
+      arr2[i + 1] = x
+    end
+    i = i + 1
+  end
+  print arr2
 end
 
 buble_sort
+
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+  left.length - right.length
+end
