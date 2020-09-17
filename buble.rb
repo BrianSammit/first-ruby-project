65c9b0d4807cb8f82272632d76b4b02fc692b44b
Nexch <mauricio.t.1999@gmail.com>
2020-09-17 11:57:20 -0400

    added the loop and if for the comparation

--- buble.rb
+++ buble.rb
@@ -1,6 +1,17 @@
-def buble_sort (arr)
+def buble_sort(arr = [1, 0, 4, 3, 5, 1, 0, 1])
   i = 0
-  while i < arr.length - 1
+  arrl = arr.length - 2
+  while i < arrl do
+    x = arr[i]
+    y = arr[i + 1]
+    if x > y
+      arr[i] = y
+      arr[i + 1] = x
+      puts 'aaa ${arr}'
+    end
+    i = i + 1
   end
   return arr
-end  
+end
+
+buble_sort
