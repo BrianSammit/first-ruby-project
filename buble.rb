9710ffa06cbd8cca364f367c4b842fe709ca79f0
Nexch <mauricio.t.1999@gmail.com>
2020-09-17 17:25:21 -0400

    fix all errors and add the deleted string

--- buble.rb
+++ buble.rb
@@ -1,4 +1,4 @@
-def buble_sort(arr = [3, 0, 5, 6, 3, 9, 0, 4])
+def buble_sort(arr = [1, 0, 4, 3, 5, 1, 0, 1])
   i = 0
   a = 1
   arrl = arr.length - 1
@@ -17,7 +17,22 @@ def buble_sort(arr = [3, 0, 5, 6, 3, 9, 0, 4])
     end
   end
   print arr
-  arr
+end
+
+def bubble_sort_by(arr2)
+  i = 0
+  arr2l = arr2.length - 1
+  while i < arr2l
+    value = yield(arr2[i], arr2[i + 1])
+    if value.positive?
+      x = arr2[i]
+      y = arr2[i + 1]
+      arr2[i] = y
+      arr2[i + 1] = x
+    end
+    i += 1
+  end
+  print arr2
 end
 
 buble_sort
