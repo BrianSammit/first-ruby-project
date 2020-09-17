9541efed58f6af69d5f78a66d9d45c5c8dd0f523
Nexch <mauricio.t.1999@gmail.com>
2020-09-17 17:26:35 -0400

    delete prints

--- buble.rb
+++ buble.rb
@@ -16,7 +16,6 @@ def buble_sort(arr = [1, 0, 4, 3, 5, 1, 0, 1])
       i += 1
     end
   end
-  print arr
 end
 
 def bubble_sort_by(arr2)
@@ -32,11 +31,9 @@ def bubble_sort_by(arr2)
     end
     i += 1
   end
-  print arr2
 end
 
 buble_sort
-
 bubble_sort_by(%w[hi hello hey]) do |left, right|
   left.length - right.length
 end
