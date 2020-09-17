5c9c46d1ad47a560da3000e4061b1cd8b81816eb
janis_jenny <pcardenasvm@gmail.com>
2020-09-17 15:38:59 -0500

    fix linter errors

--- buble.rb
+++ buble.rb
@@ -2,10 +2,10 @@ def buble_sort(arr = [3, 0, 5, 6, 3, 9, 0, 4])
   i = 0
   a = 1
   arrl = arr.length - 1
-  while a !=0  
+  while a != 0
     a = 0
     i = 0
-    while i < arrl 
+    while i < arrl
       x = arr[i]
       y = arr[i + 1]
       if x > y
@@ -17,11 +17,11 @@ def buble_sort(arr = [3, 0, 5, 6, 3, 9, 0, 4])
     end
   end
   print arr
-  return arr
+  arr
 end
 
 buble_sort
 
-bubble_sort_by(["hi","hello","hey"]) do |left,right|
+bubble_sort_by(%w['hi', 'hello', 'hey']) do |left, right|
   left.length - right.length
 end
